1699ad694d81b495e865dce2eb86b2a961d8647e
Katrin Rudisch <katrin.rudisch@gmail.com>
2016-05-30 16:52:19 +0200

    menu listView, webView

--- mobile/src/main/java/com/geowahl/geowahl/District.java
+++ mobile/src/main/java/com/geowahl/geowahl/District.java
@@ -1,27 +1,31 @@
 package com.geowahl.geowahl;
 
 import android.content.Intent;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+public class District extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_district);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(true);
 
         ListView listview = (ListView) findViewById(R.id.listView);
-        String[] values = new String[] { "Gemeinderatswahlen", "Bundespräsidentenwahlen" };
-
+        String[] values = new String[] { "alle", "Bezrik1"};
 
         ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.activity_listview, values);
 
         listview.setAdapter(adapter);
+
 
 
         listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -29,11 +33,23 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, final View view,
                                     int position, long id) {
-                Intent i = new Intent(MainActivity.this, State.class);
+                Intent i = new Intent(District.this, Webview.class);
                 startActivity(i);
                 overridePendingTransition(R.animator.activity_in, R.animator.activity_out);
             }
 
         });
+    }
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                // app icon in action bar clicked; goto parent activity.
+                //this.finish();
+                NavUtils.navigateUpFromSameTask(this);
+                overridePendingTransition(R.animator.activity_back_in, R.animator.activity_back_out);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
--- mobile/src/main/java/com/geowahl/geowahl/State.java
+++ mobile/src/main/java/com/geowahl/geowahl/State.java
@@ -1,22 +1,25 @@
 package com.geowahl.geowahl;
 
 import android.content.Intent;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+public class State extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_state);
+
 
         ListView listview = (ListView) findViewById(R.id.listView);
-        String[] values = new String[] { "Gemeinderatswahlen", "Bundespräsidentenwahlen" };
+        String[] values = new String[]{"Wien", "Niederösterreich"};
 
 
         ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.activity_listview, values);
@@ -29,11 +32,25 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, final View view,
                                     int position, long id) {
-                Intent i = new Intent(MainActivity.this, State.class);
+                Intent i = new Intent(State.this, District.class);
                 startActivity(i);
                 overridePendingTransition(R.animator.activity_in, R.animator.activity_out);
             }
 
         });
     }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                // app icon in action bar clicked; goto parent activity.
+                //this.finish();
+                NavUtils.navigateUpFromSameTask(this);
+                overridePendingTransition(R.animator.activity_back_in, R.animator.activity_back_out);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 }
+
