bd9215d27ec4eb274ada6e39b2849187c3cbc97c
maelen <mhack92.mh@gmail.com>
2016-06-01 15:07:16 +0200

    minimal view (color)

--- wear/src/main/java/com/geowahl/geowahl/MainActivity.java
+++ wear/src/main/java/com/geowahl/geowahl/MainActivity.java
@@ -11,29 +11,37 @@ import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.RectF;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.content.LocalBroadcastManager;
+import android.support.v4.view.GestureDetectorCompat;
+import android.support.wearable.view.CardFragment;
 import android.support.wearable.view.WatchViewStub;
 import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
     private TextView mTextView;
+    private WatchViewStub stub;
+    String display;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
+
+        stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
         stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
             @Override
             public void onLayoutInflated(WatchViewStub stub) {
                 mTextView = (TextView) stub.findViewById(R.id.text);
             }
         });
-
-        //setAmbientEnabled();
 
         // Register the local broadcast receiver
         IntentFilter messageFilter = new IntentFilter(Intent.ACTION_SEND);
@@ -44,45 +52,14 @@ public class MainActivity extends Activity {
     public class MessageReceiver extends BroadcastReceiver {
         @Override
         public void onReceive(Context context, Intent intent) {
-            //Log.v("myTag", "test onReceive");
             Bundle data = intent.getBundleExtra("datamap");
-            // Display received data in UI
-            String display =
-                    "Bezirk: " + data.getString("district");/* + "\n" +
-                    "Front: " + data.getString("front") + "\n" +
-                    "Middle: "+ data.getString("middle") + "\n" +
-                    "Back: " + data.getString("back");*/
+            display = data.getString("district");
+
             mTextView.setText(display);
+            stub.setBackgroundColor(Color.BLUE);
+
         }
     }
 
-    private static class SampleView extends View {
 
-        // CONSTRUCTOR
-        public SampleView(Context context) {
-            super(context);
-            setFocusable(true);
-
-        }
-
-        @Override
-        protected void onDraw(Canvas canvas) {
-
-            canvas.drawColor(Color.CYAN);
-            Paint p = new Paint();
-            // smooths
-            p.setAntiAlias(true);
-            p.setColor(Color.RED);
-            p.setStyle(Paint.Style.STROKE);
-            p.setStrokeWidth(5);
-            // opacity
-            //p.setAlpha(0x80); //
-
-            RectF rectF = new RectF(50, 20, 100, 80);
-            canvas.drawOval(rectF, p);
-            p.setColor(Color.BLACK);
-            canvas.drawArc (rectF, 90, 45, true, p);
-        }
-
-    }
 }
