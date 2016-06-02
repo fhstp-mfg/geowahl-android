7fdba582b360aa3d6e29c77d557363a10255312a
maelen <mhack92.mh@gmail.com>
2016-06-02 15:53:49 +0200

    added listview to states activity,not working yet

--- mobile/src/main/java/com/geowahl/geowahl/State.java
+++ mobile/src/main/java/com/geowahl/geowahl/State.java
@@ -1,8 +1,6 @@
 package com.geowahl.geowahl;
 
 import android.content.Intent;
-import android.graphics.Color;
-import android.graphics.Paint;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,60 +8,87 @@ import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.Toast;
 
-import java.io.Serializable;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.wearable.DataMap;
+import com.google.android.gms.wearable.Wearable;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.HashMap;
-import java.util.List;
 
-public class State extends AppCompatActivity {
+public class State extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+
+
+    private static final String TAG_NAME = "name";
+    private static final String TAG_SLUG = "slug";
+    private static String url_part1 = "http://geowahl.suits.at/";
+    private static String url_part2 = "/states";
+
+    GoogleApiClient googleClient;
+    DataMap dataMap = new DataMap();
+    String WEARABLE_DATA_PATH = "/wearable_data";
+    Button location;
+    GPSTracker gps;
+    String wahlslug;
+
+    ListView listView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_state);
 
-        ListView listview = (ListView) findViewById(R.id.listView);
-        //String[] values = new String[]{"Standort", "Wien", "Niederösterreich"};
+        Bundle b = new Bundle();
+        b = getIntent().getExtras();
+        wahlslug = b.getString("wahlSlug");
 
+        location = (Button) findViewById(R.id.location);
 
-        String test = getIntent().getStringExtra("electionId");
-        String name = getIntent().getStringExtra("partyName");
-        Integer r = Integer.parseInt(getIntent().getStringExtra("r"));
-        Integer g = Integer.parseInt(getIntent().getStringExtra("g"));
-        Integer b = Integer.parseInt(getIntent().getStringExtra("b"));
-        String a = getIntent().getStringExtra("a");
+        location.setOnClickListener(
+                new Button.OnClickListener() {
+                    public void onClick(View v) {
+                        gps = new GPSTracker(State.this);
 
-        System.out.println("...test data.." + test);
-        System.out.println("...test data.." + name);
-        System.out.println("...test data.." + r);
+                        if(gps.canGetLocation()){
+                            double latitude = gps.getLatitude();
+                            double longitude = gps.getLongitude();
 
-        View view = (View)findViewById(R.id.colorView);
-        view.setBackgroundColor(Color.rgb(r,g,b));
+                            Log.d("lat", String.valueOf(latitude));
+                            Log.d("lon", String.valueOf(longitude));
+                        }else {
+                            gps.showSettingAlerts();
+                        }
+                    }
+                }
+        );
 
-/*
-        ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.activity_listview, values);
+        String url = url_part1+wahlslug+url_part2;
+        //Log.d("url",url);
 
-        listview.setAdapter(adapter);
+        googleClient = new GoogleApiClient.Builder(this)
+                .addApi(Wearable.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
 
-//        listview.getChildAt(0).setBackgroundColor(getResources().getColor(R.color.darkblue));
-
-
-        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-
-            @Override
-            public void onItemClick(AdapterView<?> parent, final View view,
-                                    int position, long id) {
-                Intent i = new Intent(State.this, District.class);
-                startActivity(i);
-                overridePendingTransition(R.animator.activity_in, R.animator.activity_out);
-            }
-
-        });
-        */
+        getStates(url);
     }
 
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -77,6 +102,94 @@ public class State extends AppCompatActivity {
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+    public void getStates(String url_to_api) {
+        RequestQueue queue = Volley.newRequestQueue(this);
+
+        JsonArrayRequest req = new JsonArrayRequest(url_to_api,new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+
+                try {
+
+                    final ArrayList<HashMap<String, String>> arrayList = new ArrayList<HashMap<String, String>>();
+
+                    for (int i = 0; i < response.length(); i++) {
+
+                        JSONObject obj = (JSONObject) response.get(i);
+                        //Log.d("state",obj.getString(TAG_NAME).toString());
+
+                        String stateName = obj.getString(TAG_NAME);
+                        String stateSlug = obj.getString(TAG_SLUG);
+
+                        Log.d("stateName",stateName);
+                        Log.d("stateSlug",stateSlug);
+                        HashMap<String, String> d = new HashMap<>();
+                        d.put("name", stateName);
+                        d.put("slug", stateSlug);
+
+                        arrayList.add(d);
+
+                        ListAdapter adapter = new SimpleAdapter(
+                                State.this, arrayList,
+                                R.layout.activity_listview, new String[]{TAG_NAME}, new int[]{R.id.name});
+
+                        listView.setAdapter(adapter);
+                        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                            @Override
+                            public void onItemClick(AdapterView<?> parent, final View view,
+                                                    int position, long id) {
+
+                                //ausgewählte Wahl
+                                Log.d("array", arrayList.get((int)id).get(TAG_NAME));
+
+                                Intent i = new Intent(State.this, State.class);
+                                Bundle bundle = new Bundle();
+                                bundle.putString("wahlSlug",arrayList.get((int)id).get(wahlslug));
+                                bundle.putString("stateSlug",arrayList.get((int)id).get(TAG_SLUG));
+                                i.putExtras(bundle);
+                                startActivity(i);
+                                overridePendingTransition(R.animator.activity_in, R.animator.activity_out);
+                            }
+
+                        });
+
+                    }
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    Toast.makeText(getApplicationContext(),
+                            "Error: " + e.getMessage(),
+                            Toast.LENGTH_LONG).show();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(State.this, error.getMessage(),
+                        Toast.LENGTH_LONG).show();
+                VolleyLog.d("error", "Error: " + error.getMessage());
+
+            }
+        });
+
+        queue.add(req);
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
     }
 }
 
