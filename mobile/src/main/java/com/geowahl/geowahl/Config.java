6eb734bec456fc2183cb0a3aa6c80ab0524b4f98
Katrin Rudisch <katrin.rudisch@gmail.com>
2016-06-13 09:46:53 +0200

    c

---
+++ mobile/src/main/java/com/geowahl/geowahl/Config.java
@@ -0,0 +1,15 @@
+package com.geowahl.geowahl;
+
+/**
+ * Created by Katrin Rudisch on 12.06.2016.
+ */
+public class Config {
+
+    static final String TAG_NAME = "name";
+    static final String TAG_SLUG = "slug";;
+    static final String TAG_VOTES = "votes";
+    static final String TAG_PERCENT = "percent";
+    static final String TAG_EXACT = "exact";
+    static final String TAG_ID = "id";
+
+}
